97406848266f6b13947b1750f9d64ee5eeb01abb
Robert A. Blankenship <robert@theshoppad.com>
2014-09-26 21:54:00 -0700

    silly me

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ echo 'Please enter the clef user id.'
 read clef_user_id
 
 touch helpscout_creds.php
-echo '<?php'
-echo '$helpscout_key' = $helpscout_key';'> helpscout_creds.php
+echo '<?php'> helpscout_creds.php
+echo '$helpscout_key' = $helpscout_key';'>> helpscout_creds.php
 echo '$clef_app_key' = $clef_app_key';'>> helpscout_creds.php
 echo '$clef_app_secret' = $clef_app_secret';'>> helpscout_creds.php
 echo '$clef_user_id' = $clef_user_id';'>> helpscout_creds.php
-echo '?>'
+echo '?>'>> helpscout_creds.php
